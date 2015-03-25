749a22ebd620f7b4452e0054913af1d05f747391
jperucca <under.64@gmail.com>
2015-03-25 20:43:44 +0100

    [Spike] 10 last vroom messages number retrieved on server side decision, externalized

--- src/main/java/demo/web/rest/ChatRestController.java
+++ src/main/java/demo/web/rest/ChatRestController.java
@@ -3,6 +3,7 @@ package demo.web.rest;
 import demo.domain.Message;
 import demo.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,8 +18,11 @@ public class ChatRestController {
     @Autowired
     private MessageRepository messageRepository;
 
+    @Value("${vroom.messages.lasts}")
+    private int MAX_NUMBER_PAGE;
+
     @RequestMapping(method = RequestMethod.GET)
     public List<Message> pushLastMessages() {
-        return messageRepository.findAll(new PageRequest(0, 10)).getContent();
+        return messageRepository.findAll(new PageRequest(0, MAX_NUMBER_PAGE)).getContent();
     }
 }
