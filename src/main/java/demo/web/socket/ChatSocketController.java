7a5fddbae41e294db8b7caf2a79eb1fd1c221e62
jonathan_perucca <jperucca.prestataire@voyage-sncf.com>
2015-03-26 14:08:56 +0100

    [Optim] make request/reply on websocket application destination /messages subscription for initial messages data set (avoid opening/closing http communication, lighter with currently opened stomp over websocket)

--- src/main/java/demo/web/socket/ChatSocketController.java
+++ src/main/java/demo/web/socket/ChatSocketController.java
@@ -1,12 +1,17 @@
 package demo.web.socket;
 
+import java.util.List;
+
 import demo.domain.Message;
 import demo.repository.MessageRepository;
 import demo.web.dto.MessageDTO;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -18,6 +23,9 @@ public class ChatSocketController {
     @Autowired
     private Mapper mapper;
 
+	@Value("${vroom.messages.lasts}")
+	private int MAX_NUMBER_PAGE;
+
     @MessageMapping("/message")
     @SendTo("/topic/messages")
     public MessageDTO pushMessage(MessageDTO messageDTO) {
@@ -25,5 +33,10 @@ public class ChatSocketController {
         messageRepository.save(message);
         return mapper.map(message, MessageDTO.class);
     }
+
+	@SubscribeMapping("/messages")
+	public List<Message> pushLastMessages() {
+		return messageRepository.findAll(new PageRequest(0, MAX_NUMBER_PAGE)).getContent();
+	}
 
 }
